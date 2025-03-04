f4222ad11c2e9c5a8fdc523bbdb59100372450df
Oscar Topliss <97887308+OscarTopliss@users.noreply.github.com>
2025-03-04 13:47:18 +0000

    Update 2-Add-Two-Numbers.py
    Uncommented class for a demonstration

--- Medium/2-Add-Two-Numbers.py
+++ Medium/2-Add-Two-Numbers.py
@@ -1,8 +1,10 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+ Definition for singly-linked list.
+ class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         if l1 == None and l2 == None:
